0699c504820b0ba30c3e1deeb1f5c8ab207ee294
CE Igor Lino <i.lino@controlexpert.com>
2018-11-21 21:12:38 +0100

    Fix fuzzy translations missing after merge
    -Complete rewrite of merge action, with very big performance optimization (~ 300 % faster)
    -Fix fuzzy translations missing after merge
    -Fix string comparison of the msgstr array
    -A bit of more meaningful trace information
    -Added TypeScript interface description of the translation content

--- index.js
+++ index.js
@@ -1,94 +1,314 @@
 // PLUGIN_NAME: gulp-merge-po 
 var through = require('through2'),
     gutil = require('gulp-util'),
-	PoFile = require('pofile');
-    lodash = require('lodash'),
+    PoFile = require('pofile');
+var lodash = require('lodash'),
+    util = require('util'),
     path = require('path');
 
+
 /**
- * Merge .po files by convention. XX-YY is produced from XX-YY + XX if not defined in XX-YY. 
+ * Merge .po files by convention. XX-YY is produced from XX-YY + XX if not defined in XX-YY.
  * @param  {String} action Action 'merge' or 'clean' - clean sub culture file from redundant entries
  *
  * @returns {Function} A function which can be piped to files stream containing modified files
  */
-var mergePoPlugin = function(action) {
-	
-	if (action === undefined) {
-		action = 'merge';
-	}
-    
-	var poFiles = [];
-	
-	return through.obj(function(file, enc, callback) {
+var mergePoPlugin = function (action) {
 
-		var stream = this;
-			
-			if (file.isNull()) {
-				callback();
-				return;
-			}
+    var MERGE_ACTION = 'merge';
+    var CLEAN_ACTION = 'clean';
+    var LOCALE_SEPARATOR = '_';
 
-			if (file.isStream()) {
-				stream.emit('error', new gutil.PluginError('gulp-merge-po', 'Streams are not supported'));
-				callback();
-				return;
-			}
+    if (action === undefined) {
+        action = MERGE_ACTION;
+    }
 
-			var poFile = PoFile.parse(file.contents.toString())
-				
-			poFiles.push({ path: file.path, po: poFile});
+    var EMPTY_MSGSTR = [''];
 
-			callback();
-		}, function(callback) {
-			
-			var that = this;
-			
-			lodash.forEach(poFiles, function (subCultureFile) {
+    var allPoFiles = [];
 
-				var modifiedPoFile = PoFile.parse(subCultureFile.po.toString());
+    return through.obj(function onFileThrough(file, enc, callback) {
 
-				lodash.forEach(poFiles, function (cultureFile) {
-			
-					if (path.basename(subCultureFile.path, '.po').startsWith(path.basename(cultureFile.path, '.po'))) {
-								
-						modifiedPoFile.items.forEach(function (modifiedItem) {
-							cultureFile.po.items.forEach(function (cultureItem) {
-								if (strEqual(cultureItem.msgid, modifiedItem.msgid)) {
-									switch (action) {
-										case 'merge':
-										default:
-											if (strEqual(modifiedItem.msgstr, '')) {
-												modifiedItem.msgstr = cultureItem.msgstr;
-											}
-											break;
-										case 'clean':										
-											if (!strEqual(subCultureFile.path, cultureFile.path) &&
-												strEqual(cultureItem.msgstr, modifiedItem.msgstr)) {
-												modifiedItem.msgstr = '';
-											}
-											break;
-									}
-								}
-							});
-						});
+        var stream = this;
 
-						var file = new gutil.File({
-							path: path.basename(subCultureFile.path),
-							contents: new Buffer(modifiedPoFile.toString())
-						});
-		
-						that.push(file);	
-					}
+        if (file.isNull()) {
+            callback();
+            return;
+        }
 
-				});
-			});
-			
-			callback();
-		});
-		
-	function strEqual(strA, strB) {
-		return strA+'|' === strB+'|';
-	};
+        if (file.isStream()) {
+            stream.emit('error', new gutil.PluginError('gulp-merge-po', 'Streams are not supported'));
+            callback();
+            return;
+        }
+
+        var poFile = PoFile.parse(file.contents.toString())
+
+        //iteratively add the files
+        allPoFiles.push({
+            path: file.path,
+            poObj: poFile,
+            filename: path.basename(file.path, '.po'),
+            isCulture: false,
+            isSubCulture: false,
+            parentCulture: null
+        });
+
+        // TS object description interface
+        //
+        // interface IPOTranslationContent {
+        //     items: {
+        //         [id: number]: IPOTranslationItem;
+        //     };
+        // }
+        //
+        // interface IPOTranslationItem {
+        //     msgid: string;
+        //     msgctxt: string;
+        //     references: string[];
+        //     'msgid_plural': string;
+        //     msgstr: string[];
+        //     comments: string[];
+        //     extractedComments: string[];
+        //     flags: any;
+        //     obsolete: boolean;
+        // }
+
+        callback();
+    }, function onFilesReadyToProcess(callback) {
+        //when all files are available
+        var that = this;
+        console.log('Detecting culture dependency types for ' + allPoFiles.length + ' PO resource files');
+        detectCultureTypes(allPoFiles);
+        var cleanedFiles = {};
+
+        lodash.forEach(allPoFiles, function (poFile) {
+            var poFileContent = PoFile.parse(poFile.poObj.toString());
+            if (action === MERGE_ACTION) {
+                //if we need to merge anything
+                if (poFile.isSubCulture && poFile.parentCulture) {
+                    console.log('\nMerging: ' + poFile.parentCulture.filename + ' -> ' + poFile.filename);
+                    mergeParentCulture(poFile, poFileContent, poFile.parentCulture);
+                } else {
+                    console.log('\nBypassing: ' + poFile.filename);
+                }
+                cleanAllFuzzy(poFileContent);
+            } else if (action === CLEAN_ACTION) {
+                console.log('\nCleaning: ' + allPoFiles.length + ' PO resource files');
+            }
+            saveFile(poFile, poFileContent);
+        });
+
+        if (action === CLEAN_ACTION) {
+            lodash.forEach(cleanedFiles, function (value, key) {
+                console.log('file: ' + key);
+            });
+        }
+
+        callback();
+
+        function findItemByMsgId(cultureContent, msgId) {
+            var found = null;
+            lodash.forEach(cultureContent.items, function (item, key) {
+                if (strEqual(item.msgid, msgId)) {
+                    found = item;
+                    return false;
+                }
+            });
+            return found;
+        }
+
+        function getNextHighestItemKey(cultureContent) {
+            var itemKey = -1;
+            var found = null;
+            lodash.forEach(cultureContent.items, function (item, key) {
+                if (key > itemKey) {
+                    key = itemKey;
+                }
+            });
+            return itemKey + 1;
+        }
+
+        function cleanFuzzy(item) {
+            if (item && item.flags) {
+                delete item.flags.fuzzy;
+            }
+        }
+
+        function isFuzzy(item) {
+            return item && item.flags && item.flags.fuzzy === true;
+        }
+
+        function cleanAllFuzzy(poFileContent) {
+            lodash.forEach(poFileContent.items, function (cultureItem, cItemKey) {
+                cleanFuzzy(cultureItem);
+            });
+        }
+
+        function mergeParentCulture(subCulturePoFile, subCultureContent, culturePoFile) {
+            var cultureContent = PoFile.parse(culturePoFile.poObj.toString());
+            cleanAllFuzzy(cultureContent);
+            var countFuzzyOverwritten = 0;
+            var toBeAdded = [];
+            lodash.forEach(cultureContent.items, function (cultureItem, cItemKey) {
+                var subCultureItem = findItemByMsgId(subCultureContent, cultureItem.msgid);
+                //if no matching item, queue for addding it
+                if (!subCultureItem) {
+                    toBeAdded.push(cultureItem);
+                } else {
+                    var hasEmptyTranslation = areMsgStrEntryEqual(subCultureItem.msgstr, EMPTY_MSGSTR);
+                    // if (subCultureItem.msgid === 'Include roles') {
+                    //     printObjRecursive(cultureItem);
+                    //     printObjRecursive(subCultureItem);
+                    // }
+                    if (hasEmptyTranslation) {
+                        lodash.extend(subCultureItem, lodash.cloneDeep(cultureItem));
+                    } else if (isFuzzy(subCultureItem)) {
+                        countFuzzyOverwritten++;
+                        lodash.extend(subCultureItem, lodash.cloneDeep(cultureItem));
+                    }
+                }
+            });
+
+            console.log('Fuzzy translations overwritten: ' + countFuzzyOverwritten);
+
+            // seems not needed
+            // if (toBeAdded.length > 0) {
+            //     console.log('Translations to be added: ' + toBeAdded.length);
+            //     lodash.forEach(toBeAdded, function (cultureItemToAdd, idx) {
+            //         // if (idx === 0) {
+            //         //     printObjRecursive   (cultureItemToAdd);
+            //         // }
+            //         var itemKey = getNextHighestItemKey(subCultureContent);
+            //         subCultureContent[itemKey] = lodash.cloneDeep(cultureItemToAdd);
+            //     });
+            // }
+        }
+
+        function saveFile(resourceFile, contentObj) {
+            var file = new gutil.File({
+                path: path.basename(resourceFile.path),
+                contents: new Buffer(contentObj.toString())
+            });
+
+            that.push(file);
+        }
+
+        function detectCultureTypes(poFiles) {
+            //detect cultures/subculture
+            lodash.forEach(poFiles, function (poFile) {
+                if (poFile.filename.indexOf(LOCALE_SEPARATOR) > 0) {
+                    poFile.isSubCulture = true;
+                } else {
+                    poFile.isCulture = true;
+
+                }
+            });
+            lodash.forEach(poFiles, function (poFileA) {
+                lodash.forEach(poFiles, function (poFileB) {
+                    if (poFileA.filename.startsWith(poFileB.filename) && poFileA.filename !== poFileB.filename) {
+                        poFileA.parentCulture = poFileB;
+                    }
+                });
+            });
+
+            // if we need to inspect the detection
+            // lodash.forEach(poFiles, function (poFile) {
+            //     delete poFile.poObj;
+            //     printObj(poFile);
+            // });
+        }
+
+        function cleanPoItems() {
+            lodash.forEach(allPoFiles, function (subCultureFile) {
+                var subCulturePoObj = PoFile.parse(subCultureFile.poObj.toString());
+                var subCultureFilename = subCultureFile.filename;
+                lodash.forEach(allPoFiles, function (cultureFile) {
+                    var cultureFilename = cultureFile.filename;
+                    if (subCultureFilename.startsWith(cultureFilename)) {
+                        var culturePoObj = cultureFile.poObj;
+                        lodash.forEach(subCulturePoObj.items, function (subCultureItem, scItemKey) {
+                            lodash.forEach(culturePoObj.items, function (cultureItem, cItemKey) {
+                                if (strEqual(cultureItem.msgid, subCultureItem.msgid)) {
+                                    if (!strEqual(subCultureFile.path, cultureFile.path) &&
+                                        areMsgStrEntryEqual(cultureItem.msgstr, subCultureItem.msgstr)) {
+                                        subCultureItem.msgstr = EMPTY_MSGSTR;
+                                        cleanedFiles[subCultureFile.filename] = true;
+                                    }
+                                }
+                            });
+                        });
+
+                        saveFile(subCultureFile, subCulturePoObj);
+                    }
+                });
+            });
+        }
+    });
+
+    function printObj(obj) {
+        for (var name in obj) {
+            if (obj.hasOwnProperty(name)) {
+                console.log('prop(' + typeof(name) + '):\'' + name + '\' value(' + typeof(obj[name]) + '):\'' + obj[name] + '\'');
+            }
+        }
+    }
+
+    function printObjRecursive(obj) {
+        console.log(util.inspect(obj, {depth: null}));
+    }
+
+    function areMsgStrEntryEqual(entryA, entryB) {
+        entryA = extractMsgStr(entryA);
+        entryB = extractMsgStr(entryB);
+        if (!!entryA && !!!entryB) {
+            return false;
+        }
+        if (!!!entryA && !!entryB) {
+            return false;
+        }
+
+        if (!!!entryA || !!!entryB) {
+            return true;
+        }
+
+        if (entryA.toString() === entryB.toString()) {
+            return true;
+        }
+
+        return false;
+    };
+
+    function extractMsgStr(itemMsgStr) {
+        return typeof(itemMsgStr) === 'object' && typeof(itemMsgStr['0']) === 'string' ? itemMsgStr['0'] : itemMsgStr;
+    }
+
+    function strEqual(strA, strB) {
+        if (typeof(strA) === 'object') {
+            throw Exception('Unexpected object, expected string.');
+        }
+        if (typeof(strB) === 'object') {
+            throw Exception('Unexpected object, expected string.');
+        }
+        //strA = typeof(strA) === 'object' && strA['0'] &&  
+        if (!!strA && !!!strB) {
+            return false;
+        }
+        if (!!!strA && !!strB) {
+            return false;
+        }
+
+        if (!!!strA || !!!strB) {
+            return true;
+        }
+
+        if (strA.toString() === strB.toString()) {
+            return true;
+        }
+
+        return false;
+        //return strA + '|' === strB + '|';
+    };
 };
 
 module.exports = mergePoPlugin;
